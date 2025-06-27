6dcd7d993c6084dbad5d5b7b12eb7688954c32b4
nezter <nportello21@gmail.com>
2025-06-27 01:30:52 -0400

    Feat: Populate OKD setup scripts and configuration
    Populated the placeholder scripts in the `okd-setup` directory with content from the `okd-proxmox-scripts-main` reference. This includes `setup-haproxy.sh`, `setup-clients.sh`, `setup-qcow2-image.sh`, `setup-template.sh`, `clean.sh`, `delete-all.sh`, and `create-haproxy.sh`. Also, moved `install-config.yaml.ORIG` to `okd-setup/ignitions` and updated `compile-install-config.sh` to guide the user on generating ignition configs.

--- okd-setup/clean.sh
+++ okd-setup/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-# Placeholder for clean script
+rm -rf /ignitions/.openshift_install.log /ignitions/.openshift_install_state.json /ignitions/bootstrap.ign /ignitions/master.ign /ignitions/metadata.json /ignitions/worker.ign /ignitions/auth /ignitions/manifests /ignitions/openshift /ignitions/install-config.yaml
--- okd-setup/compile-install-config.sh
+++ okd-setup/compile-install-config.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-# Placeholder for install-config compilation script
+
+echo "Please edit okd-setup/ignitions/install-config.yaml.ORIG with your cluster details."
+echo "Once edited, run the following command from the okd-setup directory to generate ignition configs:"
+echo "./openshift-install create ignition-configs --dir=ignitions/"
